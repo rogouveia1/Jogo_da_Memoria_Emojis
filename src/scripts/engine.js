fd6d6f8e4ad34b5312569234632ac8e309fc16df
Rosana Gouveia <rogouveia1@gmail.com>
2024-11-16 17:09:56 -0300

    Criando a condição de vitória

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -27,7 +27,7 @@ for(let i=0; i < emojis.length; i++) {
     box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
-
+//Determino que o vetor terá apenas duas posições/cartas
 function handleClick() {
     if(openCards.length < 2) {
         this.classList.add("boxOpen");
@@ -38,7 +38,19 @@ function handleClick() {
         setTimeout(checkMatch, 500);
     }
 }
-
+//comparo as cartas, se deu match armazena senão remove
 function checkMatch(){
-    
+   if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+   } else {
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+   }
+   //limpa o vetor
+   openCards = [];
+
+   if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+    alert("Você venceu!");
+   }
 }
